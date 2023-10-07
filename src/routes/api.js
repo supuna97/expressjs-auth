bbda57f809c30df6580c92f5b5f409f8d7636b31
supuna97 <supunnanayakkara01@gmail.com>
2023-10-08 00:11:32 +0530

    Create api.js
    Co-Authored-By: luci1nana <146720584+luci1nana@users.noreply.github.com>

---
+++ src/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+
+module.exports = router;
