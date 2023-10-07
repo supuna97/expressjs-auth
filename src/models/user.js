c0af41e0567943843a05686071a5dc16be61e465
supuna97 <supunnanayakkara01@gmail.com>
2023-10-07 23:22:36 +0530

    Create user model
    Co-Authored-By: luci1nana <146720584+luci1nana@users.noreply.github.com>

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+// models/userModel.js
+const db = require("../config/database");
+
+const User = {
+  create: (user) => {
+    return db
+      .promise()
+      .execute(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        [user.username, user.email, user.password]
+      );
+  },
+
+  findByEmail: (email) => {
+    return db.promise().query("SELECT * FROM users WHERE email = ?", [email]);
+  },
+};
+
+module.exports = User;
