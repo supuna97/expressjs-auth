d1582148b01b6158a98534f01b90f1a2acf1a434
supuna97 <supunnanayakkara01@gmail.com>
2023-10-07 23:02:59 +0530

    Create mysql db connection
    Co-Authored-By: luci1nana <146720584+luci1nana@users.noreply.github.com>

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+module.exports = conn;
