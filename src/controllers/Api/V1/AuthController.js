b5c4e88108fa9daabfc4abbccbbdbfa003829812
supuna97 <supunnanayakkara01@gmail.com>
2023-10-09 20:06:16 +0530

    Update auth controller

--- src/controllers/Api/V1/AuthController.js
+++ src/controllers/Api/V1/AuthController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../../../models/User");
 const { validationResult } = require("express-validator");
 
 const authController = {
